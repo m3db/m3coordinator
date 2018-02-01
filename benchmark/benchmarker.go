3d002f836cebf203f42874846f328105ec4029be
Rob Skillington <r@uber.com>
2018-02-01 18:54:08 -0500

    Add coordination

---
+++ benchmark/benchmarker.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+)
+
+var stat = new(stats)
+
+type stats struct {
+	Writes    int64 `json:"writes"`
+	RunTimeMs int64 `json:"run_time_ms"`
+}
+
+func (s *stats) getWrites() int64 {
+	return atomic.LoadInt64(&s.Writes)
+}
+
+func (s *stats) incWrites() {
+	atomic.AddInt64(&s.Writes, 1)
+}
+
+func (s *stats) getRunTimeMs() int64 {
+	return atomic.LoadInt64(&s.Writes)
+}
+
+func (s *stats) setRunTimeMs(v int64) {
+	atomic.StoreInt64(&s.RunTimeMs, v)
+}
+
+func (s *stats) snapshot() stats {
+	return stats{Writes: s.getWrites(), RunTimeMs: s.getRunTimeMs()}
+}
+
+func (s *stats) combine(other stats) stats {
+	otherSnap := other.snapshot()
+	snap := s.snapshot()
+	return stats{
+		Writes:    snap.Writes + otherSnap.Writes,
+		RunTimeMs: snap.RunTimeMs + other.RunTimeMs,
+	}
+}
+
+// ClientOptions specify HTTP Client options.
+type HTTPClientOptions struct {
+	RequestTimeout      time.Duration `yaml:"requestTimeout"`
+	ConnectTimeout      time.Duration `yaml:"connectTimeout"`
+	KeepAlive           time.Duration `yaml:"keepAlive"`
+	MaxIdleConnsPerHost int           `yaml:"maxIdleConnsPerHost"`
+	DisableCompression  bool          `yaml:"disableCompression"`
+}
+
+// NewClient constructs a new HTTP Client.
+func NewHTTPClient(o HTTPClientOptions) *http.Client {
+	return &http.Client{
+		Timeout: o.RequestTimeout,
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			Dial: (&net.Dialer{
+				Timeout:   o.ConnectTimeout,
+				KeepAlive: o.KeepAlive,
+			}).Dial,
+			TLSHandshakeTimeout: o.ConnectTimeout,
+			MaxIdleConnsPerHost: o.MaxIdleConnsPerHost,
+			DisableCompression:  o.DisableCompression,
+		},
+	}
+}
+
+// DefaultClientOptions returns default options.
+func DefaultHTTPClientOptions() HTTPClientOptions {
+	return HTTPClientOptions{
+		RequestTimeout:      2 * time.Second,
+		ConnectTimeout:      2 * time.Second,
+		KeepAlive:           60 * time.Second,
+		MaxIdleConnsPerHost: 20,
+		DisableCompression:  true,
+	}
+}
+
+type benchmarker struct {
+	address      string
+	benchmarkers string
+}
+
+type health struct {
+	Up bool `json:"up"`
+}
+
+func (b *benchmarker) serve() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(health{Up: true})
+	})
+	mux.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("up\n"))
+		json.NewEncoder(w).Encode(stat.snapshot())
+	})
+	http.ListenAndServe(b.address, mux)
+	if err := http.ListenAndServe(b.address, mux); err != nil {
+		fmt.Fprintf(os.Stderr, "server could not listen on %s: %v", b.address, err)
+	}
+}
+
+func (b *benchmarker) allAddresses() []string {
+	var all []string
+	for _, addr := range strings.Split(b.benchmarkers, ",") {
+		addr = strings.TrimSpace(addr)
+		if addr == "" {
+			continue
+		}
+		all = append(all, addr)
+	}
+	return all
+}
+
+func (b *benchmarker) queryBenchmarkers() {
+	for {
+		var allStats []stats
+		allResponded := true
+		time.Sleep(10 * time.Second)
+		client := NewHTTPClient(DefaultHTTPClientOptions())
+		for _, addr := range b.allAddresses() {
+			req, err := http.NewRequest("GET", fmt.Sprintf("http://%s/stats", addr), nil)
+			if err != nil {
+				panic(err)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				allResponded = false
+				continue
+			}
+
+			defer resp.Body.Close()
+
+			var s stats
+			if err := json.NewDecoder(resp.Body).Decode(&s); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to decode response from benchmarker %s: %v", addr, err)
+				allResponded = false
+				continue
+			}
+
+			allStats = append(allStats, s)
+		}
+
+		if allResponded {
+			total := int64(0)
+			sumTook := time.Duration(0)
+			for _, s := range allStats {
+				total += s.Writes
+				sumTook += time.Duration(s.RunTimeMs) * time.Millisecond
+			}
+			avgTook := float64(sumTook) / float64(len(allStats))
+			avgTookDuration := time.Duration(avgTook)
+			rate := float64(total) / float64(avgTookDuration.Seconds())
+			fmt.Printf("global stats %d items in %fsec (mean values rate %f/sec)\n",
+				total, avgTookDuration.Seconds(), rate)
+		}
+	}
+}
+
+func (b *benchmarker) waitForBenchmarkers() {
+	allUp := false
+	for !allUp {
+		time.Sleep(10 * time.Millisecond)
+		allUp = true
+		client := NewHTTPClient(DefaultHTTPClientOptions())
+		for _, addr := range b.allAddresses() {
+			req, err := http.NewRequest("GET", fmt.Sprintf("http://%s/health", addr), nil)
+			if err != nil {
+				panic(err)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				allUp = false
+				continue
+			}
+
+			defer resp.Body.Close()
+
+			var r health
+			if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to decode response from benchmarker %s: %v", addr, err)
+				allUp = false
+				continue
+			}
+
+			allUp = allUp && r.Up
+		}
+	}
+
+	fmt.Printf("all ready, now synchronizing to nearest 10s...\n")
+	sync := 5 * time.Second
+	now := time.Now()
+	waitFor := now.Truncate(sync).Add(sync).Sub(now)
+	time.Sleep(waitFor)
+}
--- benchmark/load_data.go
+++ benchmark/load_data.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/m3db/m3coordinator/services/m3coordinator/config"
@@ -19,6 +20,8 @@ var (
 	m3dbClientCfg string
 	workers       int
 	namespace     string
+	address       string
+	benchmarkers  string
 
 	wg           sync.WaitGroup
 	inputDone    chan struct{}
@@ -29,6 +32,8 @@ func init() {
 	flag.StringVar(&m3dbClientCfg, "m3db-client-config", "benchmark.yml", "used to create m3db client session")
 	flag.IntVar(&workers, "workers", 1, "Number of parallel requests to make.")
 	flag.StringVar(&namespace, "namespace", "metrics", "M3DB namespace where to store result metrics")
+	flag.StringVar(&address, "address", "localhost:8888", "Address to expose benchmarker health and stats")
+	flag.StringVar(&benchmarkers, "benchmarkers", "localhost:8888", "Comma separated host:ports addresses of benchmarkers to coordinate")
 	flag.Parse()
 }
 
@@ -59,13 +64,42 @@ func main() {
 	}
 
 	itemsWritten = make(chan int)
+	var waitForInit sync.WaitGroup
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go writeToM3DB(session, ch, itemsWritten)
+		waitForInit.Add(1)
+		go func() {
+			waitForInit.Done()
+			writeToM3DB(session, ch, itemsWritten)
+		}()
 	}
 
-	start := time.Now()
-	itemsRead := addMetricsToChan(ch, metrics)
+	fmt.Printf("waiting for workers to spin up...\n")
+	waitForInit.Wait()
+	fmt.Printf("done\n")
+
+	b := &benchmarker{address: address, benchmarkers: benchmarkers}
+	go b.serve()
+	go b.queryBenchmarkers()
+	fmt.Printf("waiting for other benchmarkers to spin up...\n")
+	b.waitForBenchmarkers()
+	fmt.Printf("done\n")
+
+	var (
+		start          = time.Now()
+		itemsRead      = addMetricsToChan(ch, metrics)
+		endNanosAtomic int64
+	)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			if v := atomic.LoadInt64(&endNanosAtomic); v > 0 {
+				stat.setRunTimeMs(int64(time.Unix(0, v).Sub(start) / time.Millisecond))
+			} else {
+				stat.setRunTimeMs(int64(time.Since(start) / time.Millisecond))
+			}
+		}
+	}()
 
 	<-inputDone
 	close(ch)
@@ -79,6 +113,7 @@ func main() {
 	fmt.Println(sum)
 	end := time.Now()
 	took := end.Sub(start)
+	atomic.StoreInt64(&endNanosAtomic, int64(end.UnixNano()))
 	rate := float64(itemsRead) / float64(took.Seconds())
 
 	fmt.Printf("loaded %d items in %fsec with %d workers (mean values rate %f/sec)\n", itemsRead, took.Seconds(), workers, rate)
@@ -86,6 +121,8 @@ func main() {
 	if err := session.Close(); err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to close m3db client session, got error %v\n", err)
 	}
+
+	select {}
 }
 
 func addMetricsToChan(ch chan storage.WriteQuery, wq []storage.WriteQuery) int {
@@ -105,6 +142,8 @@ func writeToM3DB(session client.Session, ch chan storage.WriteQuery, itemsWritte
 		id := query.Tags.ID()
 		if err := session.Write(namespace, id, query.Datapoints[0].Timestamp, query.Datapoints[0].Value, xtime.Millisecond, nil); err != nil {
 			fmt.Println(err)
+		} else {
+			stat.incWrites()
 		}
 		// otherWG.Add(1)
 		// go func() {
