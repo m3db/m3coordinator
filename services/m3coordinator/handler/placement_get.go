fc7c1083234341d8a098db94ab2ada39ebb96eaf
Justin Chan <juchan@uber.com>
2018-03-12 13:49:19 -0400

    Add placement_get function

---
+++ services/m3coordinator/handler/placement_get.go
@@ -0,0 +1,69 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	m3clusterClient "github.com/m3db/m3cluster/client"
+	"github.com/m3db/m3cluster/placement"
+	"github.com/m3db/m3coordinator/generated/proto/admin"
+	"github.com/m3db/m3coordinator/util/logging"
+	"go.uber.org/zap"
+)
+
+const (
+	// PlacementGetURL is the url for the placement get handler.
+	PlacementGetURL = "/api/v1/admin/placement/get"
+)
+
+// PlacementGetHandler represents a handler for placement get endpoint.
+type PlacementGetHandler AdminHandler
+
+// NewPlacementGetHandler returns a new instance of handler.
+func NewPlacementGetHandler(clusterClient m3clusterClient.Client) http.Handler {
+	return &PlacementGetHandler{
+		clusterClient: clusterClient,
+	}
+}
+
+func (h *PlacementGetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	logger := logging.WithContext(ctx)
+
+	placement, version, err := h.placementGet(ctx)
+	if err != nil {
+		logger.Error("unable to get placement", zap.Any("error", err))
+		Error(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	placementProto, err := placement.Proto()
+	if err != nil {
+		logger.Error("unable to get placement protobuf", zap.Any("error", err))
+		Error(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	resp := &admin.PlacementGetResponse{
+		Placement: placementProto,
+		Version:   int32(version),
+	}
+
+	if err := WriteMessageResponse(w, resp, logger); err != nil {
+		Error(w, err, http.StatusInternalServerError)
+	}
+}
+
+func (h *PlacementGetHandler) placementGet(ctx context.Context) (placement.Placement, int, error) {
+	ps, err := GetPlacementServices(h.clusterClient)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	placement, version, err := ps.Placement()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return placement, version, nil
+}
